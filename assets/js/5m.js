4a55462a1a733e3bb9b29b5b1c25ff2b10f6c168
Setor de TI - Action Day <setordeti@actiondaydigital.com.br>
2026-02-05 14:51:10 -0300

    Create 5m.js

---
+++ assets/js/5m.js
@@ -0,0 +1,44 @@
+function getParam(p){
+  return new URLSearchParams(window.location.search).get(p) || "";
+}
+
+let clienteID = "", produtoID = "";
+
+window.addEventListener("DOMContentLoaded", ()=>{
+  clienteID = getParam("c");
+  produtoID = getParam("p");
+});
+
+document.getElementById("csatForm").addEventListener("submit", async (e)=>{
+  e.preventDefault();
+
+  const feedback = document.getElementById("feedbackMsg");
+  feedback.textContent = "⏳ Enviando...";
+
+  const data = {};
+  const fd = new FormData(e.target);
+
+  for (const [k, v] of fd.entries()){
+    data[k] = v;
+  }
+
+  // params
+  data.cliente = clienteID;
+  data.produto = produtoID;
+
+  try{
+    const r = await fetch("https://hook.us1.make.com/jhgz7ulsjpeqxejide9bmyeiu97ncbxs", {
+      method: "POST",
+      headers: {"Content-Type":"application/json"},
+      body: JSON.stringify(data)
+    });
+
+    feedback.textContent = r.ok ? "🎉 Respostas enviadas com sucesso!" : "⚠️ Erro ao enviar.";
+
+    if(r.ok){
+      e.target.reset();
+    }
+  }catch(err){
+    feedback.textContent = "❌ Erro de conexão.";
+  }
+});
